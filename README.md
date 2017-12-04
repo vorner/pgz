f73f3c3b7d48abc77b63a8d9d564d48b3f05a808
Michal 'vorner' Vaner <vorner@vorner.cz>
2017-12-04 21:49:12 +0100

    Configurability and error handling

--- README.md
+++ README.md
@@ -1,25 +1,23 @@
 # The parallel gzip
 
 This is an implementation of a parallel gzip. It works by splitting the input
-into chunks (currently by 100MBs, but this will be configurable in the future).
-Each chunk is compressed independently and the results are concatenated
-together. Such result can be read and decompressed by the usual gzip
-implementation.
+into chunks (by default by 32MBs, but this can be configured).  Each chunk is
+compressed independently and the results are concatenated together. Such result
+can be read and decompressed by the usual gzip implementation.
 
 The motivation is to speed up transfers of large amounts of data across a fast
 network through ssh. The ssh throughput is limited by either its compression or
 encryption routines, which are single-threaded. This allows turning compression
-of in ssh and using multiple cores to compress the data. As the decompression
+off in ssh and using multiple cores to compress the data. As the decompression
 is much faster, it is not necessary to use parallel *decompression*.
 
 ## Limitations
 
 There are certain limitations:
 
-* It is not configurable. This is because it is in an early stage of implementation.
 * The compressed representation is slightly different than from the usual
   sequential gzip. Technically, the output is multiple concatenated gzips, but
-  decompression tools commonly accepts that. Furthermore, due to the
+  decompression tools commonly accept that. Furthermore, due to the
   independent chunks, the compression ratio is likely to be a bit worse.
 * It uses more memory, to buffer the chunks.
 
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,69 @@
+extern crate failure;
 extern crate flate2;
 extern crate futures;
 extern crate futures_cpupool;
 extern crate num_cpus;
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
 
-use std::thread::{self, JoinHandle};
 use std::io::{self, Read, Write};
+use std::process;
+use std::thread::{self, JoinHandle};
 
+use failure::Error;
 use flate2::Compression;
 use flate2::bufread::GzEncoder;
 use futures::{Future, Stream, Sink};
 use futures::future;
 use futures::sync::mpsc::{self, Receiver, Sender};
 use futures_cpupool::CpuPool;
+use structopt::StructOpt;
 
-// 32 MB
-const CHUNK_SIZE: usize = 32 * 1024 * 1024;
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(short = "j", long = "jobs", help = "How many parallel jobs should be run")]
+    jobs: Option<usize>,
+    #[structopt(short = "c", long = "chunk-size",
+                help = "The size of one chunk, before it gets processed",
+                default_value = "33554432")]
+    chunk: usize,
+    #[structopt(short = "q", long = "queue-size", help = "Maximum number of waiting chunks",
+                default_value = "2")]
+    queue: usize,
+    #[structopt(short = "O", long = "compression", help = "The compression level, 0-9",
+                default_value = "3")]
+    compression: u32,
+}
 
-fn read_input(mut sink: Sender<Vec<u8>>) -> JoinHandle<()> {
-    thread::spawn(move || {
+fn read_input(mut sink: Sender<Vec<u8>>, chunk_size: usize) -> JoinHandle<Result<(), Error>> {
+    thread::spawn(move || -> Result<(), Error> {
         let stdin_unlocked = io::stdin();
         let mut stdin = stdin_unlocked.lock();
         loop {
-            let mut limit = (&mut stdin).take(CHUNK_SIZE as u64);
-            let mut buffer = Vec::with_capacity(CHUNK_SIZE);
-            limit.read_to_end(&mut buffer).unwrap();
+            let mut limit = (&mut stdin).take(chunk_size as u64);
+            let mut buffer = Vec::with_capacity(chunk_size);
+            limit.read_to_end(&mut buffer)?;
             if buffer.is_empty() {
-                return; // A real EOF
+                return Ok(()); // A real EOF
             }
-            sink = sink.send(buffer).wait().unwrap();
+            sink = sink.send(buffer).wait()?;
         }
     })
 }
 
-fn write_output(stream: Receiver<Vec<u8>>) -> JoinHandle<()> {
-    thread::spawn(move || {
-        let stdout_unlocked = io::stdout();
-        let mut stdout = stdout_unlocked.lock();
-        stream.for_each(move |chunk| {
-            stdout.write_all(&chunk).map_err(|e| panic!("{}", e))
-        }).wait().unwrap();
+fn write_output(stream: Receiver<Vec<u8>>) -> JoinHandle<Result<(), Error>> {
+    thread::spawn(move || -> Result<(), Error> {
+        stream.map_err(|()| failure::err_msg("Error on channel"))
+            .for_each(|chunk| {
+                let stdout_unlocked = io::stdout();
+                let mut stdout = stdout_unlocked.lock();
+                stdout.write_all(&chunk).map_err(Error::from)
+            }).wait()
     })
 }
 
-fn compress(i: Vec<u8>, level: Compression) -> Vec<u8> {
+fn compress(i: &[u8], level: Compression) -> Vec<u8> {
     // Pre-allocate space for all the data, compression is likely to make it smaller.
     let mut result = Vec::with_capacity(i.len());
     let mut gz = GzEncoder::new(&i[..], level);
@@ -50,23 +71,35 @@ fn compress(i: Vec<u8>, level: Compression) -> Vec<u8> {
     result
 }
 
-fn main() {
-    // TODO: Configurable amount
-    let (in_sender, in_receiver) = mpsc::channel(5);
+fn run() -> Result<(), Error> {
+    let options = Options::from_args();
+    let (in_sender, in_receiver) = mpsc::channel(options.queue);
     let (out_sender, out_receiver) = mpsc::channel(1);
-    let in_thread = read_input(in_sender);
+    let in_thread = read_input(in_sender, options.chunk);
     let out_thread = write_output(out_receiver);
 
-    let pool = CpuPool::new_num_cpus();
+    let jobs = options.jobs.unwrap_or_else(num_cpus::get);
+    let pool = CpuPool::new(jobs);
+    let compression = options.compression;
     in_receiver
+        .map_err(|()| failure::err_msg("Error on channel"))
         .map(|chunk| {
-             pool.spawn(future::lazy(move || Ok(compress(chunk, Compression::fast()))))
+             pool.spawn(future::lazy(move || {
+                 future::ok(compress(&chunk, Compression::new(compression)))
+             }))
         })
         .buffered(num_cpus::get())
-        .forward(out_sender.sink_map_err(|e| panic!("{}", e)))
-        .wait()
-        .unwrap();
+        .forward(out_sender)
+        .wait()?;
 
-    out_thread.join().unwrap();
-    in_thread.join().unwrap();
+    out_thread.join().unwrap()?;
+    in_thread.join().unwrap()?;
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
 }
